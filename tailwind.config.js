8291b60831df95ccb170684151b70e1e4d1eec10
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2024-01-26 15:59:31 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [require('flowbite/plugin')],
+}
