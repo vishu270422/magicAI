8e562e0254b279717e14c0dbc1c201616f315982
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2024-01-26 16:24:36 +0530

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    "node_modules/flowbite-react/lib/esm/**/*.js"
 ],
   theme: {
     container: {
@@ -10,5 +10,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [require("flowbite/plugin")],
 }
