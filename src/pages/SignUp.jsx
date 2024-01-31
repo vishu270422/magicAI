511f91f90148ccf3c6a3283d3aa34dfd80ad9ec0
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2024-01-31 23:00:57 +0530

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -32,7 +32,10 @@ const SignUp = () => {
       const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
       if (!passwordRegex.test(formData.password)) {
         throw new Error(
-          "Requires at least one lowercase letter,uppercase letter, special character, and number."
+          "Requires at least One Lower case Letter,
+          Uppercase Letter,
+          Special Character, 
+          and Number."
         );
       }
       if (formData.password !== formData.cpassword) {
